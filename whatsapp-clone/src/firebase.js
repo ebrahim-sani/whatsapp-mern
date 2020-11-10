b8d528e8ac76e87d9885f33f407f93c66a6cf656
ebrahim-sani <ibraheemsani035@gmail.com>
2020-11-10 03:46:21 +0100

    Add firebase

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDHeR3gmlyYiB5tQmafN5dl1siYL0kJrlU",
+  authDomain: "whatsapp-clone-b843e.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-b843e.firebaseio.com",
+  projectId: "whatsapp-clone-b843e",
+  storageBucket: "whatsapp-clone-b843e.appspot.com",
+  messagingSenderId: "293534786669",
+  appId: "1:293534786669:web:2859f08fec131f12500b64"
+};
